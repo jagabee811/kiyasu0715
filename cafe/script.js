4f0e77926ce58b26c8d00a91a4ea420f966379b5
jagabee811 <souta811812s@icloud.com>
2025-07-15 15:56:19 +0900

    Add files via upload

---
+++ cafe/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function () {
+  // 強制的に3秒間表示
+  setTimeout(function () {
+    document.getElementById('contents').style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+  }, 3200);
+});
+
+
